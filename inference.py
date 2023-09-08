4eefc43de64c1d6717006561de0c3adbd5d16369
ben18785 <ben.c.lambert@gmail.com>
2023-09-08 18:20:09 +0100

    added first results

--- inference.py
+++ inference.py
@@ -27,6 +27,11 @@ def create_mcmc_filename(error_model_type):
     file_extra = error_model_type
     filename = 'mcmc_results_' + file_extra + ".pickle"
     return filename
+
+def create_nested_filename(error_model_type):
+    file_extra = error_model_type
+    filename = 'nested_results_' + file_extra + ".pickle"
+    return filename
     
 
 def optimise(model, values, times, error_model_type):
@@ -162,13 +167,58 @@ def inference(model, values, times, error_model_type):
     pickle.dump((xs, pints.GaussianLogLikelihood, log_prior,
                  chains, 'HaarioBardenetACMC'), open(filename, 'wb'))
 
+def inference_nested(model, values, times, error_model_type):
+
+    problem = pints.SingleOutputProblem(model, times, values)
+
+    if error_model_type=="independent":
+        print("Fitting independent errors model...")
+        log_likelihood = pints.GaussianLogLikelihood(problem)
+        lower_bounds = independent_lower_bounds
+        upper_bounds = independent_upper_bounds
+        log_prior = pints.UniformLogPrior(lower_bounds, upper_bounds)
+    elif error_model_type=="ar1":
+        print("Fitting AR1 model...")
+        log_likelihood = pints.AR1LogLikelihood(problem)
+        lower_bounds = ar1_lower_bounds
+        upper_bounds = ar1_upper_bounds
+        log_prior = pints.UniformLogPrior(lower_bounds, upper_bounds)
+    elif error_model_type=="student_t":
+        print("Fitting Student-t model...")
+        log_likelihood = pints.StudentTLogLikelihood(problem)
+        lower_bounds = student_t_lower_bounds
+        upper_bounds = student_t_upper_bounds
+        log_prior = pints.UniformLogPrior(lower_bounds, upper_bounds)
+    elif error_model_type=="arma11":
+        log_likelihood = pints.ARMA11LogLikelihood(problem)
+        lower_bounds = arma11_lower_bounds
+        upper_bounds = arma11_upper_bounds
+        log_prior = pints.UniformLogPrior(lower_bounds, upper_bounds)
+
+    # Create mcmc routine with four chains
+    mcmc = pints.NestedController(log_likelihood, log_prior)
+
+    # Add stopping criterion
+    mcmc.set_iterations(2000)
+
+    # Run!
+    chains = mcmc.run()
+
+    # Save chains for plotting and analysis
+    filename = create_nested_filename(error_model_type)
+    pickle.dump((xs, pints.GaussianLogLikelihood, log_prior,
+                 chains, 'NestedMCMC'), open(filename, 'wb'))
+
 
 if __name__ == '__main__':
     model = SingleReactionSolution()
     data = ECTimeData('GC01_FeIII-1mM_1M-KCl_02_009Hz.txt', model,
                       ignore_begin_samples=5, ignore_end_samples=0, samples_per_period=200)
     
-    error_models = ['arma11', 'student_t']
+    error_models = ['independent', 'ar1', 'arma11', 'student_t']
     for em in error_models:
         optimise(model, data.current, data.times, em)
         inference(model, data.current, data.times, em)
+        # inference_nested(model, data.current, data.times, em)
+
+        
